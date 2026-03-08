eebdcbcaafff32894faa81fd13dad2dfdd802bdb
minhlu14206 <minhlu14206@gmail.com>
2026-03-08 18:42:38 +0100

    update

--- order/customer.js
+++ order/customer.js
@@ -13,7 +13,7 @@ export async function sendOrderStatusToCustomer(customerId, orderId, status, ima
             token: customerId, // Assuming customerId is the FCM token, adjust if it's not
             title: "Order Status Update",
             body: `Your order #${orderId} is now ${status}.`,
-            link: `${process.env.SERVER_URL}/receipt/production?orderId=${orderId}`, 
+            link: `${process.env.APP_URL}/receipt/production?orderId=${orderId}`, 
             icon: icon ,
             image: image
             //badge: "https://th.bing.com/th/id/OIP.yN8LzEPl81YvvqO2ZW-91AHaE8?w=243&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
