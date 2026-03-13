23a263f6615bd71604ce31c73be1ff9c36d7ac6a
minhlu14206 <minhlu14206@gmail.com>
2026-03-13 11:57:17 +0100

    update

--- payment/index.js
+++ payment/index.js
@@ -20,6 +20,7 @@ export async function HandleCreatePayment(order) {
     }
     catch(error){
         log.err(`[💳❌]Error occurred while initiating payment for order ID: ${order.id}`, error);
+        console.error(error);
         return false;
     }
 
