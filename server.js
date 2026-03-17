be36d8bf9c393a3c6df21a117ea5206388539fa3
taply365 <minhlu14206@gmail.com>
2026-03-17 23:03:03 +0100

    update

--- server.js
+++ server.js
@@ -20,14 +20,9 @@ app.use(cookieParser());
 
 // important for cookies from frontend
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://taply.dk",
-        "https://www.taply.dk",
-        "https://dev.taply.dk",
-    ],
-    credentials: true,
-    methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"]
+  origin: origins,
+  credentials: true,
+  methods: [ "GET" , "POST" , "PUT" , "DELETE" , "PATCH" , "OPTIONS" ]
 }));
 
 app.get('/connection', (req, res) => {
