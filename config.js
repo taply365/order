ced2173e6379d18312b56c4ad827981075b6bfee
taply365 <minhlu14206@gmail.com>
2026-04-04 00:33:23 +0200

    updae

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
-  SOCKET_PORT: Number(process.env.SOCKET_PORT) || 1269,
-  SERVER_PORT: Number(process.env.SERVER_PORT) || 1268,
+  SOCKET_PORT: Number(process.env.SOCKET_PORT) || 3004,
+  SERVER_PORT: Number(process.env.SERVER_PORT) || 4000,
 };
 
 export const origins = [
