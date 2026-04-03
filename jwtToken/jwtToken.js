e98b5238edc483e3f1ef70918855f66f12294ed9
taply365 <minhlu14206@gmail.com>
2026-04-04 00:27:41 +0200

    updaye

--- jwtToken/jwtToken.js
+++ jwtToken/jwtToken.js
@@ -9,6 +9,5 @@ export const jwtMiddleware = expressjwt({
     secret: SECRET_KEY,           // Secret key to verify the token
     algorithms: ["HS256"],       // Specify the algorithm used to sign the token
 }).unless({
-    method: ["OPTIONS"],
     path: [/^\/gen-guest-token\/.*/, "/gen-guest-token"]// Exclude these routes from JWT verification (public routes)
 });
